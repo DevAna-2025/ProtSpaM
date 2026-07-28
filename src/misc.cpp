f855223070f253440c1002e6dbc005a97f700a8c
Ana Izaguirre <ana.cimatamoros@gmail.com>
2026-07-28 04:30:38 -0600

    fix: emove time_elapsed function
    Removed the time_elapsed function that calculated and printed execution time.

--- src/misc.cpp
+++ src/misc.cpp
@@ -223,28 +223,7 @@ vector<vector<char> > rand_pattern(int w, int d, int pat_number)
     return pattern;
 }
 
-void time_elapsed(double start)
-{
-    double finished = omp_get_wtime();
-    double exec_time = finished - start;
 
-    int hours = exec_time / 3600;
-    int minutes = (exec_time - (3600 * hours) ) / 60;
-    int seconds = exec_time - (3600 * hours) - (60 * minutes);
-
-    if (hours != 0)
-    {
-        cout << "Time elapsed: " << hours << "h" << minutes << "m" << seconds << "s\n";
-    }
-    else if (minutes != 0)
-    {
-        cout << "Time elapsed: " << minutes << "m" << seconds << "s\n";
-    }
-    else
-    {
-        cout << "Time elapsed: " << exec_time << "s\n";
-    }
-}
 
 void outputDistanceMatrix (const vector<Species>& species, const string& fileName,
                            const vector<vector<double> >& distanceMatrix, const bool& tooDistant) {
