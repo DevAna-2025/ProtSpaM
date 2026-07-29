7cc6b04e6bcf4e62949ca6fec25361bd4adc06b7
Ana Izaguirre <ana.cimatamoros@gmail.com>
2026-07-29 04:00:08 -0600

    Refactor parseParameters and printParameters signatures

--- include/parameters.h
+++ include/parameters.h
@@ -6,8 +6,8 @@
 #include <vector>
 #include <fstream>
 
-    void parseParameters(int argc, char *argv[], int& weight, int& dc, int& threshold, int& patterns, int& threads, std::vector<std::string>& inputFileNames, std::string& output, bool& sp, std::string& lp, bool& outputScores);
+    void parseParameters(int argc, char *argv[], int& weight, int& dc, int& threshold, int& patterns, std::vector<std::string>& inputFileNames, std::string& output, bool& sp, std::string& lp, bool& outputScores);
     void printHelp();
-    void printParameters(int&, int&, int&, int&, int&, std::string&);
+    void printParameters(int&, int&, int&, int&, std::string&);
 
 #endif // PARAMETERS_H
